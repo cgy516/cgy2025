e141732de2bdc746ac1c75ff8b055cff1264dfc2
cgy516 <153485410+cgy516@users.noreply.github.com>
2025-09-29 09:04:35 +0800

    Update album.js

--- js/album.js
+++ js/album.js
@@ -8,7 +8,7 @@ window.onload=function(){
         var lbli=document.createElement('li');
         var lbimg=document.createElement('img');
 
-        lbimg.src="../cgy2025/image/pics0"+i+".jpg";
+        lbimg.src="../image/pics0"+i+".jpg";
         lbimg.style.width="750px"
         lbimg.style.height="421px"
         lbli.appendChild(lbimg);
@@ -121,3 +121,4 @@ function dotmov(){
 
 
 }
+
